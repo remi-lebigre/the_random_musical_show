a548b1ddb164caed4129d9b749dd03839e7ca82e
remi <remi@148.fr>
2015-12-27 18:22:16 +0100

    meme version que celle en ligne

--- rem.js
+++ rem.js
@@ -1,10 +1,14 @@
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
 
-
-
-function randomizator(){
-    var rand_artists = parseInt(Math.random()*a_artists.length);
-    var rand_prefixes = parseInt(Math.random()*a_prefixes.length);
-    var rand_genres = parseInt(Math.random()*a_genres.length);
+var rand_artists=0, rand_prefixes=0, rand_genres=0;
+function randomizator(artist,prefix,genre){
+    rand_artists = artist || parseInt(Math.random()*a_artists.length);
+    rand_prefixes = prefix || parseInt(Math.random()*a_prefixes.length);
+    rand_genres = genre || parseInt(Math.random()*a_genres.length);
     var newStyle = document.createElement('style');
 	var font = a_genres[rand_genres][1];
 	font += (font == "font_8bit") ? ".otf" : ".ttf";
@@ -28,12 +32,8 @@ function randomizator(){
     document.getElementById("results").innerHTML = "<p class='lead' style='"+text_transform+"font-family:"+ a_genres[rand_genres][1]+"'>"+a_artists[rand_artists][0] + "<br>" +  a_prefixes[rand_prefixes] + "<br>" +  a_genres[rand_genres][0];
 	document.getElementById("results_container").style.background ="url(src/back/"+a_genres[rand_genres][2]+".jpg) center center / cover no-repeat";	
 	document.getElementById("img_artist").src ="src/compositeurs/"+a_artists[rand_artists][1]+".png";
-}
-
-
-
-function start(){
-
+	
+	$('#results p').fitText({ maxFontSize: '32px' });
 }
 
 /*
@@ -52,3 +52,27 @@ function save_dat_shit(){
    canvas.parentElement.removeChild(canvas);
 });
 }*/
+var clip = null;
+$(function(){
+	var params = $_GET('p');
+	if(params){
+	var a_rands = params.split(',');
+	if(a_rands.length == 1) a_rands = params.split('%2C');
+		randomizator(parseInt(a_rands[0]),parseInt(a_rands[1]),parseInt(a_rands[2]));
+	}
+	clip = new Clipboard('#share');
+});
+
+function share(){
+	var text = 'http://lebig.re/sites/randommusicalshow/?p='+rand_artists+','+rand_prefixes+','+rand_genres;
+	clip.text = function() {
+            return text;
+        }
+    clip.on('success',function(){
+		  clip.off('success');
+	  $('#share').before('<div class="btn btn-success">Copié ! Colle le lien sur les réseaux</div>');
+	  $('.btn-success').delay(1000).fadeOut(300,function(){
+		  $(this).remove();
+	  });    
+	});
+}
